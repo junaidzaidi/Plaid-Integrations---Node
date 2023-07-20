dfa99fffd87702783160c78c36c823c0cac9f860
Junaid Zaidi <51705693+junaidzaidi@users.noreply.github.com>
2023-07-20 12:17:00 -0700

    Updated Readme

--- Readme.txt
+++ Readme.txt
@@ -6,3 +6,10 @@ PLAID TOKENS
 3. Send PUBLIC_TOKEN  from backend to plaid API and receive ACCESS_TOKEN
 4. Provide this ACCESS_TOKEN to authentication API which would give the account and routing numbers.
 5. Also store ACCESS_TOKEN in the database.
+
+
+HOW TO RUN:
+
+1. Run Server. (Server running on port 8000)
+2. Run Client. (Client should run on port 5173)
+3. Click the button "Connect a bank account"
--- server/index.js
+++ server/index.js
@@ -93,17 +93,3 @@ app.post("/hello", (request, response) => {
 
 
 app.listen(port, () => console.log(`Server has started listening on port ${port}`));
-
-
-
-/*
-
-    PLAID TOKENS
-
-    1) Get the LINK_TOKEN from the plaid through backend and provide it to frontend.
-    2) Then user will do the authentication. Once authenticated we will give PUBLIC_TOKEN to backend through frontend
-    3) Send PUBLIC_TOKEN  from backend to plaid API and receive ACCESS_TOKEN
-    4) Provide this ACCESS_TOKEN to authentication API which would give the account and routing numbers.
-    5) Also store ACCESS_TOKEN in the database.
-
-*/
